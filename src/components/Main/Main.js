395d90e8908d058e9ba75aa4e646ad4991b2b08e
JustinMartin7x <justin.martin7x@gmail.com>
2021-02-07 15:09:39 -0800

    what i have so far

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,14 +4,32 @@ import styles from './Main.css';
 import Form from '../Form/Form';
 
 export default function Main() {
-  const [input, setInput] = useState('api url');
+  const [input, setInput] = useState(
+    'futuramaapi.herokuapp.com/api/v2/characters'
+  );
   const [call, setCall] = useState('get');
   const [send, setSend] = useState('');
+  const [response, setResponse] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(input, call, send);
+  const handleSubmit = () => {
+    const result = fetchApi(input, call, send);
+    console.log('!!!!!!!!!!!!!!', result);
+    setResponse(result.text);
   };
+
+  const fetchApi = (url, method, body) => {
+    if (method !== 'GET') {
+      return fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body,
+      }).then((res) => res.json());
+    }
+    return fetch(url).then((res) => res.json());
+  };
+  console.log(response);
 
   return (
     <div className={styles.main}>
@@ -21,7 +39,7 @@ export default function Main() {
         setSend={setSend}
         handleSubmit={handleSubmit}
       />
-      this is the main page
+      <div className={styles.resultSection}>{response}</div>
     </div>
   );
 }

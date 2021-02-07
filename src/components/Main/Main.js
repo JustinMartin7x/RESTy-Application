2cf50c7cec981f6745f12da324916dd8f640c418
JustinMartin7x <justin.martin7x@gmail.com>
2021-02-07 13:24:16 -0800

    added form component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Main.css';
+
+import Form from '../Form/Form';
 
 export default function Main() {
-  return <div>This is the Main Component</div>;
+  const [input, setInput] = useState('api url');
+  const [call, setCall] = useState('get');
+  const [send, setSend] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input, call, send);
+  };
+
+  return (
+    <div className={styles.main}>
+      <Form
+        setInput={setInput}
+        setCall={setCall}
+        setSend={setSend}
+        handleSubmit={handleSubmit}
+      />
+      this is the main page
+    </div>
+  );
 }
